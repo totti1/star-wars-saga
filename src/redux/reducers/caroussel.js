f74ce9f72ff44b5ce38d3357f5595ff227b84542
Aristote Katy <stotestorch@gmail.com>
2022-02-02 08:54:32 +0200

    Caroussel done for the home page

--- src/redux/reducers/caroussel.js
+++ src/redux/reducers/caroussel.js
@@ -2,40 +2,53 @@ const carousselData = (state, action) => {
 return [
     {
       imageSrc:
-        'https://images.unsplash.com/photo-1559508551-44bff1de756b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80',
-      title: 'Studio Room',
-      description: 'Lorem ipsum dolor sit amet, consectur dolori',
-      pricingText: 'USD 50/Day',
-      features: ['Free Wifi', 'Free breakfast'],
+      'https://i0.wp.com/thefutureoftheforce.com/wp-content/uploads/2019/10/img_3686.jpg?fit=1920%2C1080&ssl=1',
+      title: 'Star Wars I, The Phantom Menace',
     },
     {
       imageSrc:
-        'https://images.unsplash.com/photo-1616940844649-535215ae4eb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-      title: 'Deluxe Room',
-      description: 'Lorem ipsum dolor sit amet, consectur dolori',
-      pricingText: 'USD 80/Day',
-      features: ['Free Wifi', 'Free breakfast'],
+        'https://aiptcomics.com/wp-content/uploads/2017/12/4c70bfe75d96721d356bb68dcd0cd005-star-wars-episode-ii-attack-of-the-clones-1468112543.jpg',
+      title: 'Star Wars II, Attack of the Clone',
     },
     {
       imageSrc:
-        'https://images.unsplash.com/photo-1599619351208-3e6c839d6828?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80',
-      title: 'King Deluxe Room',
-      description: 'Lorem ipsum dolor sit amet, consectur dolori',
-      pricingText: 'USD 150/Day',
-      features: ['Free Wifi', 'Free breakfast', 'Discounted Meals'],
+        'https://jumpcutonline.co.uk/wp-content/uploads/2020/05/3840x2160-68-scaled-1200x675.jpg',
+      title: 'Star Wars III, Revenge of the Sith',
     },
     {
       imageSrc:
-        'https://images.unsplash.com/photo-1461092746677-7b4afb1178f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
-      title: 'Royal Suite',
-      description: 'Lorem ipsum dolor sit amet, consectur dolori',
-      pricingText: 'USD 299/Day',
-      features: [
-        'Free Wifi',
-        'Free breakfast',
-        'Discounted Meals',
-        "MacBook for work use (hotel's property)",
-      ],
+        'https://wallpapercave.com/wp/NMaWhm1.jpg',
+      title: 'Star Wars IV, A New Hope',
+    },
+    {
+      imageSrc:
+      'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/32fc1bb80652d99c479632e26a86682fdd21c407ec4ecb4f8664cb1d682bbee2._UY500_UX667_RI_V_TTW_.jpg',
+      title: 'Star Wars V, The Empire Strikes back',
+    },
+    {
+      imageSrc:
+        'https://images5.alphacoders.com/111/1115524.jpg',
+      title: 'Star Wars VI, Return of the Jedi',
+    },
+    {
+      imageSrc:
+        'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/826040f5-466d-4fe5-ad72-dbcdfd174c26/db3lu2w-e9082aba-238d-4f06-a918-319f0d74a5a0.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzgyNjA0MGY1LTQ2NmQtNGZlNS1hZDcyLWRiY2RmZDE3NGMyNlwvZGIzbHUydy1lOTA4MmFiYS0yMzhkLTRmMDYtYTkxOC0zMTlmMGQ3NGE1YTAucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.7LXYN8GUUNqWBYzThrMh9suj_1SbkfrEoRx_b8DEbVY',
+      title: 'Star Wars VII, The Force Awakens',
+    },
+    {
+      imageSrc:
+        'https://wallpaperaccess.com/full/3999093.jpg',
+      title: 'Star Wars VII, The Last Jedi',
+    },
+    {
+      imageSrc:
+        'https://www.islandecho.co.uk/wp-content/uploads/2019/11/Star-Wars-Episode-IX-9-The-Rise-of-Skywalker.jpg',
+      title: 'Star Wars IX, The Rise Of Skywalker',
+    },
+    {
+      imageSrc:
+        'https://mocah.org/mocahbig/77823-Star-WarsRogue-One-A-Star-Wars-Story-4k-Ultra-HD-Wallpaper.jpg',
+      title: 'Star Wars Rogue One',
     },
   ]
 }
