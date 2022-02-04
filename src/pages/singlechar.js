59bacb568567c503c92e97bda7d1f07195ee1c7e
Aristote Katy <stotestorch@gmail.com>
2022-02-05 00:23:26 +0200

    Single Character page done

--- src/pages/singlechar.js
+++ src/pages/singlechar.js
@@ -1,118 +1,117 @@
-import { useEffect, useState, useRef } from "react"
+import { useEffect, useState } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { loadSingleCharacter } from "../redux/reducers/singleChar";
-import LinearProgress from '@mui/material/LinearProgress';
+import { LinearProgress, IconButton } from '@mui/material';
 import { useParams } from 'react-router-dom';
-import { Paper, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import { FilmReviewCard } from "../components";
 import axios from "axios";
-import { loadFilms } from "../redux/reducers/films";
-// import { LetterAvatars } from "../components";
+import "./styles/singleChar.css"
+import KeyboardArrowDownTwoToneIcon from '@mui/icons-material/KeyboardArrowDownTwoTone';
+import * as Scroll from 'react-scroll';
 
+let scroll    = Scroll.animateScroll;
 
 export default function SingleChar (){
-
+    const [filmsloaded, setFilmsloaded] = useState(false)
+    const [movies, setMovies] = useState([])
     let { id } = useParams();
-    const films = [];
     const dispatch = useDispatch()
     const myData = useSelector(state => state)
-    const isFirstUpdate = useRef(true);
+    // const trackButton = useRef();
+
     useEffect(() => {
         if(parseInt(id) >= 17){
             id= parseInt(id)+1
         }
         dispatch(loadSingleCharacter(id))
-        if (isFirstUpdate.current) {
-            console.log(myData.singleChar.list.films)
-            isFirstUpdate.current = false;
-            return;
-        }
-        // for(let i of myData.singleChar.list.films){
-        //     dispatch(loadFilms(id))
-        //     axios.get(i)
-        //     .then(res => {
-        //         const persons = res.data;
-        //         console.log(persons)
-        //     })
-        // }
     }, [dispatch]);
-    // useEffect(() => {
-    //     if (isFirstUpdate.current) {
-    //     isFirstUpdate.current = false;
-    //     return;
-    //     }
-
-    //     console.log('componentDidUpdate');
-    // });
-
     const { list } = myData.singleChar
-    if(myData.singleChar.loading === false){
-        // for(let i of list.films){
-        //     loadFilms(id)
-        //     axios.get(i)
-        //     .then(res => {
-        //         const persons = res.data;
-        //         films.push(persons)
-        //         // setFilms(res.data)
+
+    const getLevel = () => {
+        if(list.films){
+            let promiseArray = list.films.map( url=> axios.get(url) );
+            Promise.all( promiseArray )
+            .then(
+                results => {
+                const gistsDescriptions = results.map( el => el.data );
+                setFilmsloaded(true)
+                setMovies(gistsDescriptions)
                 
-        //     })
-        // }
-        // console.log(myData.films)
+                }
+            )
+            .catch(console.log)
+            scroll.scrollToBottom();
+        }
+        
+    }
+    console.log(movies)
+    console.log(filmsloaded)
+    
+    if(myData.singleChar.loading === false){
+
         return(
-            <div className="container">
-                <h2>Let me Introduce you to,</h2>
-                <div className="row">
-                    <div className="col-md-3">
-                        <Paper 
-                            elevation={8} 
-                            sx={{ 
-                                // minHeight: '80vh', 
-                                maxWidth: 345, 
-                                margin:'auto', 
-                                display: 'flex', 
-                                alignItems: 'center',
-                                flexDirection: 'column',
-                                padding: 3
-                            }}>
-                            <Typography gutterBottom variant="h4">{list.name}</Typography>
-                            <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
-                                Birth Year: {list.birth_year}
-                            </Typography>
-                            <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
-                                Gender: {list.gender}
-                            </Typography>
-                            <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
-                                Eye Color: {list.eye_color}
-                            </Typography>
-                            <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
-                                Hair Color: {list.hair_color}
-                            </Typography>
-                            <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
-                                Skin Color: {list.skin_color}
-                            </Typography>
-                            <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
-                                Height: {list.height}
-                            </Typography>
-                            <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
-                                Mass: {list.mass}
-                            </Typography>
-                            
-                        </Paper>
+            <div>
+                <div className="bg-img">
+                    <Typography gutterBottom variant="h4" sx={{ fontSize: 36, fontWeight: 'bold' }}>
+                        Let me Introduce you to,
+                    </Typography>
+                    <Typography gutterBottom variant="h4" sx={{ fontSize: 30, fontWeight: 'bold' }}>
+                        {list.name}
+                    </Typography>
+                    <Typography sx={{ fontSize: 18, fontWeight: 800 }} gutterBottom>
+                        Birth Year: {list.birth_year}
+                    </Typography>
+                    <Typography sx={{ fontSize: 18, fontWeight: 800 }} gutterBottom>
+                        Gender: {list.gender}
+                    </Typography>
+                    <Typography sx={{ fontSize: 18, fontWeight: 800 }} gutterBottom>
+                        Eye Color: {list.eye_color}
+                    </Typography>
+                    <Typography sx={{ fontSize: 18, fontWeight: 800 }} gutterBottom>
+                        Hair Color: {list.hair_color}
+                    </Typography>
+                    <Typography sx={{ fontSize: 18, fontWeight: 800 }} gutterBottom>
+                        Skin Color: {list.skin_color}
+                    </Typography>
+                    <Typography sx={{ fontSize: 18, fontWeight: 800 }} gutterBottom>
+                        Height: {list.height}
+                    </Typography>
+                    <Typography sx={{ fontSize: 18, fontWeight: 800 }} gutterBottom>
+                        Mass: {list.mass}
+                    </Typography>
+                    <IconButton 
+                        aria-label="arrow" 
+                        sx={{color: 'white', fontSize: 50, marginTop: 10 }}
+                        onClick={()=> getLevel()}
+                    >
+                        <KeyboardArrowDownTwoToneIcon fontSize="inherit" />
+                    </IconButton>
+                </div>
+                <div className="container" style={{ "margin": "auto"}} >
+                    <Typography 
+                        gutterBottom 
+                        variant="h4" 
+                        sx={{ fontSize: 28, fontWeight: 'bold', margin: 5 }}
+                    >
+                        Movies Featured in,
+                    </Typography>
+                    <div className="row">
+                        {filmsloaded ? 
+                            movies.map((item, index)=>{
+                            return (
+                                <div className="col-md-3">
+                                    <FilmReviewCard
+                                        avatar={item.title.slice(0,1)}
+                                        title={item.title}
+                                        date={item.release_date}
+                                        description={item.opening_crawl}
+                                    />
+                                </div>
+                                )
+                        }) : <LinearProgress /> }
                     </div>
-                    <div className="col-md-8">
-                        {films.map((item, index)=>{
-                            console.log(films)
-                            if(!films){
-                                console.log(films)
-                                return <LinearProgress />
-                            }
-                            else{
-                                console.log(films)
-                                return <FilmReviewCard />
-                            }
-                        })}
-                        
-                    </div>
+                    
                 </div>
                 
             </div>
