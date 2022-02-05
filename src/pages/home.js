96c45502d00d8a068fd53184183e5975d66a25cf
Aristote Katy <stotestorch@gmail.com>
2022-02-05 15:50:20 +0200

    changes

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import React,{ useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { ImageCarousel } from "../components"
 import carousselData from "../redux/reducers/caroussel"
